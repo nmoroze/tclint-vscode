afc83550d8242a7e2b7b6e4902a30326deb4e5af
Noah Moroze <noahmoroze@gmail.com>
2025-01-04 22:15:06 -0500

    Remove Python tests
    The placeholders test LSP functionality directly, but this is subsumed
    by tests in the tclint project itself.

--- noxfile.py
+++ noxfile.py
@@ -36,13 +36,6 @@ def _check_files(names: List[str]) -> None:
 
 def _update_pip_packages(session: nox.Session) -> None:
     session.run("pip-compile", "--generate-hashes", "--resolver=backtracking", "--upgrade", "./requirements.in")
-    session.run(
-        "pip-compile",
-        "--generate-hashes",
-        "--resolver=backtracking",
-        "--upgrade",
-        "./src/test/python_tests/requirements.in",
-    )
 
 
 def _get_package_data(package):
@@ -102,39 +95,22 @@ def setup(session: nox.Session) -> None:
 
 
 @nox.session()
-def tests(session: nox.Session) -> None:
-    """Runs all the tests for the extension."""
-    session.install("-r", "src/test/python_tests/requirements.txt")
-    session.run("pytest", "src/test/python_tests")
-
-
-@nox.session()
 def lint(session: nox.Session) -> None:
     """Runs linter and formatter checks on python files."""
     session.install("-r", "./requirements.txt")
-    session.install("-r", "src/test/python_tests/requirements.txt")
 
     session.install("pylint")
     session.run("pylint", "-d", "W0511", "./bundled/tool")
-    session.run(
-        "pylint",
-        "-d",
-        "W0511",
-        "--ignore=./src/test/python_tests/test_data",
-        "./src/test/python_tests",
-    )
     session.run("pylint", "-d", "W0511", "noxfile.py")
 
     # check formatting using black
     session.install("black")
     session.run("black", "--check", "./bundled/tool")
-    session.run("black", "--check", "./src/test/python_tests")
     session.run("black", "--check", "noxfile.py")
 
     # check import sorting using isort
     session.install("isort")
     session.run("isort", "--check", "./bundled/tool")
-    session.run("isort", "--check", "./src/test/python_tests")
     session.run("isort", "--check", "noxfile.py")
 
     # check typescript code
